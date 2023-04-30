f1995db88aba07b3df8e561b484fb9470eb690a4
Ianes Filho <116043505+ianesfilho@users.noreply.github.com>
2023-04-30 20:01:17 -0300

    Add files via upload

---
+++ TrabalhoMyFlightPOO/MyFlight_base/MyFlight_base/src/pucrs/myflight/modelo/Aeronave.java
@@ -0,0 +1,32 @@
+package pucrs.myflight.modelo;
+
+public class Aeronave implements Contavel{
+	private String codigo;
+	private String descricao;
+	
+	public Aeronave(String codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public void qtdObjetos() {
+		
+	}
+	
+}
---
+++ TrabalhoMyFlightPOO/MyFlight_base/MyFlight_base/src/pucrs/myflight/modelo/Contavel.java
@@ -0,0 +1,6 @@
+package pucrs.myflight.modelo;
+
+    interface Contavel {
+        void qtdObjetos();
+    }
+
